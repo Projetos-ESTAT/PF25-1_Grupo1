8c9e2b49ed8e988eb9c2f0c970be920912149588
joaofabioalc <joaofabioalc@gmail.com>
2025-04-18 02:02:08 -0300

    analise 2

--- rdocs/joaofabio.R
+++ rdocs/joaofabio.R
@@ -24,42 +24,125 @@ source("rdocs/source/packages.R")
 # ---------------------------------------------------------------------------- #
 
 banco <- read.csv('inflacao.csv', sep = ',', dec = '.')
-dados <- subset(banco, ano >= 2002 & ano <= 2022)
-View(dados)
 
-# Modelo
-modelo <- lm(juros_reais ~ selic_meta, dados)
+# Análise 1
+
+dados1 <- subset(banco, ano >= 2002 & ano <= 2022)
+View(dados1)
+
+## Modelo
+modelo <- lm(juros_reais ~ selic_meta, dados1)
 summary(modelo)
 
-# Coeficiente de correlação de Pearson
-r <- cor(dados$selic_meta, dados$juros_reais)
+## Coeficiente de correlação de Pearson
+r <- cor(dados1$selic_meta, dados1$juros_reais)
 
-# R²
+## R²
 r2 <- summary(modelo)$r.squared
 
 ## Gráficos
-grafic1 <- ggplot(dados, aes(x = selic_meta, y = juros_reais)) +
-  geom_jitter(colour = "#A11D21", size = 3) +
+
+grafic1 <- ggplot(dados1, aes(x = selic_meta, y = juros_reais)) +
+  geom_jitter(colour = "#A11D21", size = 3, alpha = 0.3) +
   labs(
     x = "Taxa Selic",
     y = "Juros Reais",
-    title = "Relação entre SELIC e Juros Reais (2002–2022)"
   ) +
   theme_estat()
 
-grafic2 <- ggplot(dados, aes(x = selic_meta, y = juros_reais)) +
-  geom_jitter(colour = "#A11D21", size = 3) +
+grafic2 <- ggplot(dados1, aes(x = selic_meta, y = juros_reais)) +
+  geom_jitter(colour = "#A11D21", size = 3, alpha = 0.3) +
   geom_smooth(method = "lm", se = FALSE, color = "#003366", size = 1.2) +
   labs(
     x = "Taxa Selic",
     y = "Juros Reais",
-    title = "Relação entre SELIC e Juros Reais (2002–2022)"
   ) +
-  annotate("text", x = 8, y = 14, label = paste0("r = ", round(r, 2)), size = 5) +
-  annotate("text", x = 8, y = 13, label = paste0("R² = ", round(r2, 2)), size = 5) +
+  theme_estat()
+
+## Quadro
+print_quadro_resumo(dados1, selic_meta)
+print_quadro_resumo(dados1, juros_reais)
+
+# Análise 2
+
+dados2 <- banco %>%
+  mutate(referencia = as.Date(paste0(referencia, "-01")))
+
+dados_filtrados <- dados2 %>%
+  filter(referencia >= as.Date("2002-01-01") & referencia <= as.Date("2022-12-01"))
+
+view(dados2)
+
+dados_long <- dados_filtrados %>%
+  select(referencia, ipca_variacao, ipca15_variacao) %>%
+  pivot_longer(cols = c(ipca_variacao, ipca15_variacao),
+               names_to = "índice",
+               values_to = "variacao")
+
+
+dados_long$índice <- recode(dados_long$índice,
+                            "ipca_variacao" = "IPCA",
+                            "ipca15_variacao" = "IPCA-15")
+
+dados_dezembro <- dados2 %>%
+  filter(month(referencia) == 12 & year(referencia) >= 2002 & year(referencia) <= 2022) %>%
+  mutate(ano = year(referencia)) %>%
+  select(ano, ipc_fipe_acumulado_ano)
+
+view(dados_dezembro)
+
+
+## Modelo
+modelo2 <- lm(ipca_variacao ~ ipca15_variacao, dados1)
+summary(modelo2)
+
+## Coeficiente de correlação de Pearson
+r_2 <- cor(dados1$ipca15_variacao, dados1$ipca_variacao)
+
+## R²
+r2_2 <- summary(modelo2)$r.squared
+
+## Gráficos
+
+grafic3 <- ggplot(dados_long) +
+  aes(x = referencia, y = variacao, group = índice, colour = índice) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  scale_colour_manual(name = "Índice", values = c("IPCA" = "#A11D21", "IPCA-15" = "#003366")) +
+  labs(x = "Ano",
+       y = "Variação (%)") +
+  theme_estat()
+
+grafic4 <- ggplot(dados_filtrados) +
+  aes(x = referencia, y = ipc_fipe_acumulado_ano, group = 1) +
+  geom_line(size = 1, colour = "#A11D21") +
+  geom_point(colour = "#A11D21", size = 2) +
+  labs(
+    x = "Ano",
+    y = "Variação Acumulada (%)"
+  ) +
+  theme_estat()
+
+grafic5 <- ggplot(dados1, aes(x = ipca15_variacao, y = ipca_variacao)) +
+  geom_jitter(colour = "#A11D21", size = 3, alpha = 0.3) +
+  labs(
+    x = "IPCA15",
+    y = "IPCA",
+  ) +
+  theme_estat()
+
+grafic6 <- ggplot(dados1, aes(x = ipca15_variacao, y = ipca_variacao)) +
+  geom_jitter(colour = "#A11D21", size = 3, alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE, color = "#003366", size = 1.2) +
+  labs(
+    x = "IPCA15",
+    y = "IPCA",
+  ) +
   theme_estat()
 
 
-# Quadro
-print_quadro_resumo(dados, selic_meta)
-print_quadro_resumo(dados, juros_reais)
+
+## Quadro
+print_quadro_resumo(dados1, ipca_variacao)
+print_quadro_resumo(dados1, ipca15_variacao)
+print_quadro_resumo(dados_dezembro, ipc_fipe_acumulado_ano)
