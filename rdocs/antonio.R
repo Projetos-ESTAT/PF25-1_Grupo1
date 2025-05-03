c6986a1d47a2820343ff603c781d8a4688662b81
Antônio Olímpio Gonçalves de Oliveira Pinto <toni.olimpio14@gmail.com>
2025-05-02 23:49:24 -0300

    entrega final

--- rdocs/antonio.R
+++ rdocs/antonio.R
@@ -30,7 +30,7 @@ analise_temporal <- analise_1 %>%
 
 
 #Grafico
-ggplot(analise_temporal) +
+analise1_graf <- ggplot(analise_temporal) +
   geom_line(aes(x = ano, y = Inflacao12, color = "Inflação (Doze meses)"),linewidth = 1) +
   geom_point(aes(x = ano, y = Inflacao12), size = 1) +
   geom_line(aes(x = ano, y = Selic, color = "Taxa SELIC"),linewidth = 1) +
@@ -54,7 +54,7 @@ correlacao <- cor(analise_2$ipca_acumulado_doze_meses, analise_2$selic_meta, met
 
 #Grafico
 
-marmota <- ggplot(analise_2) +
+analise2_graf <- ggplot(analise_2) +
   aes(x = selic_meta, y = ipca_acumulado_doze_meses) +
   geom_point(colour = "#A11D21", size = 3) +
   labs(
@@ -69,19 +69,19 @@ ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
 
 #Praparando a 
 analise_3 <- data.frame(inflacao_analises[,2], inflacao_analises[,25])
-analise_3[1:12,1] <- "F.H.C." 
-analise_3[13:60,1] <- "Lula 1"
-analise_3[61:108,1] <- "Lula 2"
-analise_3[109:156,1] <- "Dilma"
-analise_3[157:204,1] <- "Temer"
+analise_3[1:12,1] <- "FHC" 
+analise_3[13:60,1] <- "Lula1"
+analise_3[61:108,1] <- "Lula2"
+analise_3[109:156,1] <- "Dilma1"
+analise_3[157:204,1] <- "Dilma2/Temer"
 analise_3[205:252,1] <- "Bolsonaro"
 
 #Separando os mandatos para a análise para a tabela
-FHC_Medidas <- filter(analise_3, ano == "F.H.C.")
-Lula1_Medidas <- filter(analise_3, ano == "Lula 1")
-Lula2_Medidas <- filter(analise_3, ano == "Lula 2")
-Dilma_Medidas <- filter(analise_3, ano == "Dilma")
-Temer_Medidas <- filter(analise_3, ano == "Temer")
+FHC_Medidas <- filter(analise_3, ano == "FHC")
+Lula1_Medidas <- filter(analise_3, ano == "Lula1")
+Lula2_Medidas <- filter(analise_3, ano == "Lula2")
+Dilma_Medidas <- filter(analise_3, ano == "Dilma1")
+Temer_Medidas <- filter(analise_3, ano == "Dilma2/Temer")
 Bolso_Medidas <- filter(analise_3, ano == "Bolsonaro")
 
 #Medidas
@@ -90,18 +90,16 @@ describe(FHC_Medidas$salario_minimo)
 sd(FHC_Medidas$salario_minimo)
 quantile(FHC_Medidas$salario_minimo, 0.25)
 quantile(FHC_Medidas$salario_minimo, 0.75)
-print_quadro_resumo(analise_3$salario_minimo)
 
-analise_3[,2] <- as.factor(analise_3[,2])
-describe(Lula_Medidas1$salario_minimo)
-sd(Lula_Medidas1$salario_minimo)
-quantile(Lula_Medidas1$salario_minimo, 0.25)
-quantile(Lula_Medidas1$salario_minimo, 0.75)
+describe(Lula1_Medidas$salario_minimo)
+sd(Lula1_Medidas$salario_minimo)
+quantile(Lula1_Medidas$salario_minimo, 0.25)
+quantile(Lula1_Medidas$salario_minimo, 0.75)
 
-describe(Lula_Medidas2$salario_minimo)
-sd(Lula_Medidas2$salario_minimo)
-quantile(Lula_Medidas2$salario_minimo, 0.25)
-quantile(Lula_Medidas2$salario_minimo, 0.75)
+describe(Lula2_Medidas$salario_minimo)
+sd(Lula2_Medidas$salario_minimo)
+quantile(Lula2_Medidas$salario_minimo, 0.25)
+quantile(Lula2_Medidas$salario_minimo, 0.75)
 
 describe(Dilma_Medidas$salario_minimo)
 mean(Dilma_Medidas$salario_minimo)
@@ -121,7 +119,7 @@ quantile(Bolso_Medidas$salario_minimo, 0.25)
 quantile(Bolso_Medidas$salario_minimo, 0.75)
 #Gráfico
 
-analise3_graf <- ggplot(analise_3) +
+analise3_box <- ggplot(analise_3) +
   aes(x = reorder(ano, salario_minimo, FUN = median), y = salario_minimo) +
   geom_boxplot(fill = c("#A11D21"), width = 0.5) +
   stat_summary(
